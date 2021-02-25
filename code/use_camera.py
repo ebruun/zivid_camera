e5ae428b809b7dfdebb11a00db8302d254e5708f
EdvardBruun <ebruun@princeton.edu>
2021-02-25 14:21:27 -0500

    added .zdf files back in, camera/emulated scripts in code

---
+++ code/use_camera.py
@@ -0,0 +1,50 @@
+"""Capture sample."""
+# PYTHON IMPORTS
+from pathlib import Path
+import datetime
+import zivid
+
+# LOCAL IMPORTS
+from utils import get_sample_data_path_edvard as get_sample_data_path
+from utils import get_output_data_path_edvard as get_output_data_path
+
+
+def _main():
+    app = zivid.Application()
+    camera = app.connect_camera()
+
+    settings = zivid.Settings()
+    settings.acquisitions.append(zivid.Settings.Acquisition())
+    settings.acquisitions[0].aperture = 5.6
+    settings.acquisitions[0].exposure_time = datetime.timedelta(microseconds=8333)
+    settings.processing.filters.outlier.removal.enabled = True
+    settings.processing.filters.outlier.removal.threshold = 5.0
+
+    print("Capturing frame")
+    with camera.capture(settings) as frame:
+        data_file = Path() / get_output_data_path() / "output_frame.zdf"
+        print(f"Saving frame to file: {data_file}")
+        frame.save(data_file)
+
+    print("Configuring 2D settings")
+    settings_2d = zivid.Settings2D()
+    settings_2d.acquisitions.append(zivid.Settings2D.Acquisition())
+    settings_2d.acquisitions[0].exposure_time = datetime.timedelta(microseconds=30000)
+    settings_2d.acquisitions[0].aperture = 11.31
+    settings_2d.acquisitions[0].brightness = 1.80
+    settings_2d.acquisitions[0].gain = 2.0
+    settings_2d.processing.color.balance.red = 1.0
+    settings_2d.processing.color.balance.green = 1.0
+    settings_2d.processing.color.balance.blue = 1.0
+    settings_2d.processing.color.gamma = 1.0
+
+    print("Capturing 2D frame")
+    with camera.capture(settings_2d) as frame_2d:
+        image = frame_2d.image_rgba()
+
+        data_file = Path() / get_output_data_path() / "output_frame.png"
+        print(f"Saving frame to file: {data_file}")
+        image.save(data_file)    
+
+if __name__ == "__main__":
+    _main()
---
+++ code/utils.py
@@ -0,0 +1,29 @@
+"""
+Utilities for working with Zivid Camera
+"""
+
+import os
+from pathlib import Path
+
+
+def get_sample_data_path_edvard():
+    """Get sample data path.
+
+    Returns:
+        path: Sample data path
+
+    """
+
+    path = Path(__file__).parents[1] / "ZividSampleData2"
+    return path
+
+def get_output_data_path_edvard():
+    """Get output data path.
+
+    Returns:
+        path: Output data path
+
+    """
+
+    path = Path(__file__).parents[1] / "output"
+    return path
