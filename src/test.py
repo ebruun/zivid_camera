5e8a12228a3024a4fb4e083eaa448a2bd0a76f38
EdvardBruun <ebruun@princeton.edu>
2021-06-01 14:09:46 -0400

    got the axis draw to work for sample images

--- src/test.py
+++ src/test.py
@@ -1,15 +1,24 @@
 import numpy as np
 import cv2 as cv
 import glob
+import copy
+
+from numpy.lib.function_base import copy
 
 # LOCAL IMPORTS
 from src.utility.io import create_file_path
+
+from compas.geometry import Point
+from compas.geometry import Vector
+from compas.geometry import Frame
+
+from compas.geometry import Transformation
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = 30*np.zeros((6*7,3), np.float32)
+objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
@@ -45,83 +54,50 @@ for fname in images:
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 
-#############################################
 
-#############################################
+#############################
+i = 2
 
- #Input images
-img1 = cv.imread(create_file_path("img_calibration","left04.jpg"))
-ret1, corners1 = cv.findChessboardCorners(img1, (7,6))
+a,_ = cv.Rodrigues(rvecs[i])
+b = tvecs[i]
 
-img2 = cv.imread(create_file_path("img_calibration","left02.jpg"))
-ret2, corners2 = cv.findChessboardCorners(img2, (7,6))
+H = np.insert(a,3,np.squeeze(b),axis=1)
+H = np.insert(H,3,[0,0,0,1],axis=0)
 
-cv.drawChessboardCorners(img1, (7,6), corners1, ret1)
-cv.imshow('img', img1)
+T = Transformation.from_matrix(H.tolist())
+F = Frame.from_transformation(T)
+
+print("trans matrix", T)
+print("frame", F)
+
+img1 = cv.imread(images[i])
+
+h,  w = img1.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+img1_nodistort = cv.undistort(img1, mtx, dist, None, newcameramtx)
+
+def draw_axis(img, r, t, K):
+    # unit is mm
+    rotV, _ = cv.Rodrigues(r)
+    print(rotV)
+    points = np.float32([[10, 0, 0], [0, 10, 0], [0, 0, 10], [0, 0, 0]]).reshape(-1, 3)
+    axisPoints, _ = cv.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
+
+    axisPoints = axisPoints.astype(int)
+
+    img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (0,0,255), 3)
+    img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
+    img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (255,0,0), 3)
+    return img
+
+img1_axis = draw_axis(copy(img1),rvecs[i],tvecs[i],mtx)
+img1_nodistort_axis = draw_axis(copy(img1_nodistort),rvecs[i],tvecs[i],newcameramtx)
+
+
+h1 = np.concatenate((img1, img1_axis), axis = 1)
+h2 = np.concatenate((img1_nodistort, img1_nodistort_axis), axis = 1)
+v1 = np.concatenate((h1, h2), axis = 0)
+
+cv.imshow('img', v1)
 cv.waitKey(0)
-
-if ret1 and ret2:
-    print("found both")
-
-obj_pnt = np.zeros((6*7,3), np.float32)
-obj_pnt[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-obj_pnt_planar = np.delete(obj_pnt,obj=2, axis=1)
-
-ret, rvec1, tvec1 = cv.solvePnP(obj_pnt,corners1, mtx, dist)
-print(rvec1, tvec1)
-
-R,_ = cv.Rodrigues(rvec1)
-
-print(R)
-
-
-# #img_pnt = cv.undistortPoints(corners1, mtx, dist)
-# img_pnt = corners1
-
-# H,_ = cv.findHomography(obj_pnt_planar, img_pnt)
-
-# print(H)
-# norm = np.linalg.norm(H[:,0])
-
-# H /= norm
-# print(H, norm)
-
-
-# c1 = H[:,0]
-# c2 = H[:,1]
-# c3 = np.cross(c1,c2)
-# tvec = H[:,2]
-
-# print(c1,c2,c3,tvec)
-
-# R =np.array([c1,c2,c3])
-# R = np.squeeze(R.T)
-
-# print(R)
-
-# W, U, Vt = cv.SVDecomp(R)
-
-# R = U*Vt
-
-# def draw_axis(img, R, t, K):
-#     # unit is mm
-#     rotV, _ = cv.Rodrigues(R)
-#     points = np.float32([[10, 0, 0], [0, 10, 0], [0, 0, 10], [0, 0, 0]]).reshape(-1, 3)
-#     axisPoints, _ = cv.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
-
-    
-#     axisPoints = abs(axisPoints.astype(int))
-#     axisPoints = axisPoints.astype(int)
-#     print(axisPoints)
-#     img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (0,0,255), 3)
-#     img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
-#     img = cv.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (255,0,0), 3)
-#     return img
-
-# im = draw_axis(img1,R,R.dot(tvec),mtx)
-
-# cv.imshow('img', im)
-# cv.waitKey(0)
-
-# print(R)
-
